afc7461e3ad9254adc689c7afaeef0d1516b3d63
jresinas <jresinas@emergya.com>
2017-07-14 14:31:08 +0200

    Muestra y cambia a tipo numérico el campo 'tiempo ANS' cuando se genera el CSV del listado de peticiones

--- lib/tee/queries_helper_patch.rb
+++ lib/tee/queries_helper_patch.rb
@@ -25,6 +25,25 @@ module TEE
 		    value = Issue.get_total_time(issue.id, @project)
 		when column.name == :total_time_last_status
 		    value = Issue.get_total_time_last_status(issue.id, @project)
+		when "cf_#{Setting.plugin_tiempo_entre_estados[:tee_time_ans]}".to_sym
+			start_statuses = issue.get_start_statuses_ans(issue)
+			pause_statuses = issue.get_pause_statuses_ans(issue)
+			intervals = issue.get_intervals
+
+			start_statuses.each do |role, statuses_start|
+				intervals.each do |interval|
+					if @status_ans_id.include?(interval[:status_id]) && statuses_start.map{|s| s[:id]}.include?(interval[:status_id])
+						time = TeeTimetable.get_total_time(issue.project_id, role, interval[:start], interval[:end])
+						if time != 0
+							time_hours = Issue.get_hours(time)
+							if time_hours > 0.0
+								# return Issue.get_seconds_to_hh_mm(time)
+								return time.round.to_s
+							end
+						end
+					end
+				end
+			end
 		else
 		    value = column.value_object(issue)
 		    if value.is_a?(Array)
